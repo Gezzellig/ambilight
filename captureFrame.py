3031043cef55382d306d1ed36a57a6be4727f234
Gezzellig <knaap@live.com>
2019-01-29 21:59:00 +0100

    working colorFlicker

--- captureFrame.py
+++ captureFrame.py
@@ -5,7 +5,9 @@ import numpy as np
 class FaultyFrameException(Exception):
     pass
 
+
 arduino = serial.Serial("/dev/ttyUSB0", 115200)
+
 
 def selectColor(height, width, frame):
     return frame[height][width]
@@ -14,26 +16,35 @@ def selectColor(height, width, frame):
 def calcLedlocations(frame_height, frame_width, numleds_l, numleds_t, numleds_r):
     led_locations = list()
 
-    #left border
+    # left border
     for i in range(0, numleds_l):
-        led_locations.append((int((i+0.5)*(float(frame_height)/numleds_l)), 0))
+        led_locations.append((int((i + 0.5) * (float(frame_height) / numleds_l)), 0))
 
-    #top border
+    # top border
     for i in range(0, numleds_t):
-        led_locations.append((0, int((i+0.5)*(float(frame_height)/numleds_t))))
+        led_locations.append((0, int((i + 0.5) * (float(frame_height) / numleds_t))))
 
-    #right border
+    # right border
     for i in range(0, numleds_l):
-        led_locations.append((int((i+0.5)*(float(frame_height)/numleds_l)), frame_width-1))
+        led_locations.append((int((i + 0.5) * (float(frame_height) / numleds_l)), frame_width - 1))
 
     return led_locations
+
 
 def byterize_led_colors(led_colors):
     byte_led_colors = list()
     for colors in led_colors:
         for color in colors:
-            byte_led_colors.append(chr(color))
+            byte_led_colors.append(bytes(color))
     return byte_led_colors
+
+
+def concat_led_colors(led_colors):
+    concat_led_colors_list = list()
+    for colors in led_colors:
+        for color in colors:
+            concat_led_colors_list.append(color)
+    return concat_led_colors_list
 
 
 cap = cv2.VideoCapture(0)
@@ -47,42 +58,46 @@ print("Frame height: {} width:{}".format(frame_width, frame_heigth))
 
 ret, frame = cap.read()
 
-colors = [[255,0,0],[0,0,255],[255,0,0],[0,0,255]]
+colors = [[255, 0, 0], [0, 0, 255], [255, 0, 0], [0, 0, 255]]
+print("Wat we gaan sturen")
 print(colors)
-print(byterize_led_colors(colors))
-print(240)
-print(frame[2][2])
-print(type(frame[2][2]))
-arduino.write(byterize_led_colors(colors))
+print("byterize")
+concat_colors = concat_led_colors(colors)
+byte_colors = np.array(concat_colors).astype(np.uint8)
+print("dit")
+print(byte_colors)
+arduino.write(str(byterize_led_colors(colors)))
+
 
 def main_loop():
-	while(True):
-		# frame is a simple RGB frame[height][width][R][G][B]
-		ret, frame = cap.read()
-		if not ret:
-			raise FaultyFrameException("The frame was not properly loaded")
+    while (True):
+        # frame is a simple RGB frame[height][width][R][G][B]
+        ret, frame = cap.read()
+        if not ret:
+            raise FaultyFrameException("The frame was not properly loaded")
 
-		led_colors = list()
-		print("hier",len(led_locations))
-		for h, w in led_locations:
-			led_colors.append(selectColor(h, w, frame))
+        led_colors = list()
+        print("hier", len(led_locations))
+        for h, w in led_locations:
+            led_colors.append(selectColor(h, w, frame))
 
-		#led colors must be pushed to the arduino somehow
-		#print(led_colors)
-		#print(type(led_colors)))
-		print("scherm")
-		print(len(led_colors))
+        # led colors must be pushed to the arduino somehow
+        # print(led_colors)
+        # print(type(led_colors)))
+        print("scherm")
+        bytearray = led_colors.astype(np.uint8)
+        print(len(led_colors))
 
-		arduino.write(byterize_led_colors(led_colors))
-		
+        arduino.write(byterize_led_colors(led_colors))
 
-		# Display the resulting frame
-		cv2.imshow('frame', frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-	# When everything done, release the capture
-	cap.release()
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+
 
 main_loop()
 
-#cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
--- colorFlicker.py
+++ colorFlicker.py
@@ -3,18 +3,36 @@ import time
 import serial
 import numpy as np
 
+num_leds = 5
+
 arduino = serial.Serial("/dev/ttyUSB0", 115200)
 
-def byterize_led_colors(led_colors):
+def concat_led_colors(led_colors):
     return np.append([], led_colors)
+
 print("sleep")
 time.sleep(4)
-for i in range(0,500):
-    print(i)
-    colors = [[170,0,255],[0,0,i],[i,0,0],[i,i,i]]
-    nparray = byterize_led_colors(colors)
-    bytearray = nparray.astype(np.uint8)
-    arduino.write(bytearray)
+print("send")
+
+def send_colors(colors):
+    concat_colors = concat_led_colors(colors)
+    byte_colors = concat_colors.astype(np.uint8)
+    print(byte_colors)
+    arduino.write(byte_colors)
     arduino.flush()
+
+def turn_off_leds():
+    turned_off = [0,0,0]
+    colors = []
+    for i in range(0, num_leds):
+        colors.append(turned_off)
+    send_colors(colors)
+
+for j in range(0,256):
+    i = 255 - j
+    print(i)
+    colors = [[1, 1, 1],[0,0,i],[i,i,i],[i,i,i], [20,20,20]]
+    send_colors(colors)
     time.sleep(0.01)
+turn_off_leds()
 
